4eb4c9e8c46bc428c610a398e0d3a611ccb64f81
Anirudha Bele <beleanirudha8@gmail.com>
2024-12-13 20:48:11 +0530

    change old API and update the new API

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -6,10 +6,10 @@ function useCurrencyInfo(currency) {
 
 	useEffect(() => {
 		fetch(
-			`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+			`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`
 		)
 			.then((res) => res.json())
-			.then((res) => setData(res));
+			.then((res) => setData(res[currency]));
 	}, [currency]);
 	return data;
 }
