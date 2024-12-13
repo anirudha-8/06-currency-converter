913df55dc26d36814ec912d1ac79f26dc8f5df89
Anirudha Bele <beleanirudha8@gmail.com>
2024-12-13 12:50:11 +0530

    add 'index.js' in 'components' folder for production level use case scenario

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+// Production level way of importing and exporting components
+// create "index.js" in component folder
+// import all necessary components
+// export them all at once
+
+import InputBox from "./InputBox";
+// import SearchBox from "./SearchBox";
+// import Card from "./Card";
+// .
+// .
+// .
+// . . . . import all components
+
+export { InputBox };
+
+// export them all at once, like this comma separated
+// export { InputBox, SearchBox, Card };
